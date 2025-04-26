0e08e23d1b997c7a3b7a9ba4cb9f4b67e848ddce
boykartajati31 <kartajatiboy@gmail.com>
2025-04-26 18:31:23 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Boy Ulfha</h1>
+    <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam voluptas sunt, nobis quis doloremque nam! Fugiat rerum quos odio, laboriosam doloremque mollitia odit itaque enim cupiditate recusandae illo ratione reprehenderit.</h1>
 </body>
 </html>
